c2f2d8389dd1e87d8a326b989d12dd37d08a0212
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-05-02 22:14:32 +0530

    all types of uses of useState hook

--- use-state-hook/src/App.jsx
+++ use-state-hook/src/App.jsx
@@ -3,14 +3,50 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [task, setTask] = useState(["hello", "aditya"]);// for arrays
+  const [user, setUser] = useState({ name: "Aditya", age: 21 }); // for object
 
+  const handleTask = () => {
+    const newTask = "Go for a walk";
+    setTasks([...task, newTask]);
+  }
+
+  const changeName = () => {
+    setUser({ ...user, name: "Harsh" }); // Update only the name
+  };
   return (
     <>
+
+{/* // simple set values      */}
       <div className="card">
         <button onClick={() => setCount(count + 1)}>count is {count}</button>
       </div>
+
+{/* // Now when we want to update the arrays   */}
+<div>
+      <h3>Tasks:</h3>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+      <button onClick={addTask}>Add Task</button>
+    </div>
+
+{/* // Now when we want to update the object  */}
+
+<div>
+      <h3>User Profile:</h3>
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+      <button onClick={changeName}>Change Name</button>
+    </div>
+
+
     </>
   );
 }
 
 export default App;
+
+
