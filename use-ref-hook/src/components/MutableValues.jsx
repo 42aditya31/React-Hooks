9fa4c72a1ff06aadad7897eb644cf68aee4c3065
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-05-04 22:53:20 +0530

    Explain the use of useRef with mutable values

---
+++ use-ref-hook/src/components/MutableValues.jsx
@@ -0,0 +1,24 @@
+import React, { useRef, useState } from "react";
+
+function Timer() {
+  const [seconds, setSeconds] = useState(0);
+  const timerRef = useRef(null);
+
+  const startTimer = () => {
+    timerRef.current = setInterval(() => {
+      setSeconds(prev => prev + 1);
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    clearInterval(timerRef.current);
+  };
+
+  return (
+    <div>
+      <h2>Seconds: {seconds}</h2>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Stop</button>
+    </div>
+  );
+}
